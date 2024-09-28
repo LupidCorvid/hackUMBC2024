62eaf5d49c1741319a937c1db13859b2a9968071
LupidCorvid <lucidcanine921@gmail.com>
2024-09-28 14:13:43 -0400

    more movement work

--- New Unity Project (1)hackUMBC2024/Assets/playerMovement.cs
+++ New Unity Project (1)hackUMBC2024/Assets/playerMovement.cs
@@ -7,9 +7,9 @@ public class playerMovement : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField] PhysicsMaterial2D frictionNotMoving;
     [SerializeField] PhysicsMaterial2D frictionMoving;
-    float acceleration = .5f;
-    float jumpSpeed = 2;
-    float maxSpeed = 4;
+    public float acceleration = 2;
+    public float jumpSpeed = 5;
+    public float maxSpeed = 8f;
     bool isJumping = false;
 
     void Start()
@@ -19,30 +19,41 @@ public class playerMovement : MonoBehaviour
     
     void FixedUpdate()
     {
+        print(rb.velocity.x);
         inputChecker();
     }
 
     void inputChecker()
     {
         Vector2 inputMovement = Vector2.zero;
-        if (Input.GetKeyDown(KeyCode.W))
+        
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
         {
-            if (!isJumping)
+            if (!isJumping) 
             {
                 inputMovement += new Vector2(0, jumpSpeed);
                 isJumping = true;
             }
-            
         }
+        //Half the upward velocity when the player lets go of the jump key
+        if ((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space)) && isJumping)
+        {
+            inputMovement = new Vector2(0, (.5f*jumpSpeed));
+        }
+
+        //Sideways movement
         if (Input.GetKey(KeyCode.A))
         {
-            if (inputMovement.x <= -maxSpeed) inputMovement = new Vector2(-maxSpeed, 0);
-            else inputMovement += new Vector2(-acceleration, 0);
+            if (rb.velocity.x > -maxSpeed) inputMovement += new Vector2(-acceleration, rb.velocity.y);
+            //if (rb.velocity.x <= -maxSpeed) inputMovement = new Vector2(-maxSpeed, 0);
+            //else inputMovement += new Vector2(-acceleration, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            if (inputMovement.x >= maxSpeed) inputMovement = new Vector2(maxSpeed, 0);
-            else inputMovement += new Vector2(acceleration, 0);
+            if (rb.velocity.x < maxSpeed) inputMovement += new Vector2(acceleration, rb.velocity.y);
+            //if (rb.velocity.x >= maxSpeed) inputMovement = new Vector2(maxSpeed, 0);
+            //else inputMovement += new Vector2(acceleration, 0);
         }
         if (inputMovement == Vector2.zero)
             rb.sharedMaterial = frictionNotMoving;
@@ -52,6 +63,11 @@ public class playerMovement : MonoBehaviour
         rb.velocity += inputMovement;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isJumping = false;
+    }
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
