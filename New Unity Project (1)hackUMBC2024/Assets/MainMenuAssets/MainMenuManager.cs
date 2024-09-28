b6e6b86500a37c1db1cbf75d9c7ca7037cc8fc82
RJWMD <113476572+RJWMD@users.noreply.github.com>
2024-09-28 14:29:22 -0400

    Fixed quit button not working, made it exit play mode if in play mode

--- New Unity Project (1)hackUMBC2024/Assets/MainMenuAssets/MainMenuManager.cs
+++ New Unity Project (1)hackUMBC2024/Assets/MainMenuAssets/MainMenuManager.cs
@@ -47,6 +47,11 @@ public class MainMenuManager : MonoBehaviour
 
     public void QuitGame()
     {
+#if UNITY_STANDALONE
         Application.Quit();
+#endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
