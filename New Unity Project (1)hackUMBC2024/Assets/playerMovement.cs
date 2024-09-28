cf2aa3c24c44624e72fb724c87c8fa8ba3cc50ba
LupidCorvid <lucidcanine921@gmail.com>
2024-09-28 14:49:33 -0400

    More movement code + animations

--- New Unity Project (1)hackUMBC2024/Assets/playerMovement.cs
+++ New Unity Project (1)hackUMBC2024/Assets/playerMovement.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
     [SerializeField] PhysicsMaterial2D frictionNotMoving;
     [SerializeField] PhysicsMaterial2D frictionMoving;
     public float acceleration = 2;
-    public float jumpSpeed = 5;
+    public float jumpSpeed = 8;
     public float maxSpeed = 8f;
     bool isJumping = false;
+    bool isFalling = false;
 
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        anim = gameObject.GetComponent<Animator>();
     }
     
     void FixedUpdate()
     {
-        print(rb.velocity.x);
         inputChecker();
     }
 
@@ -28,35 +32,37 @@ public class playerMovement : MonoBehaviour
         Vector2 inputMovement = Vector2.zero;
         
         //Jumping
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W) && !isJumping)
         {
-            if (!isJumping) 
-            {
-                inputMovement += new Vector2(0, jumpSpeed);
-                isJumping = true;
-            }
+            anim.Play("jumpUp");
+            inputMovement += new Vector2(0, jumpSpeed);
+            isJumping = true;
+            sr.color = Color.red;
         }
         //Half the upward velocity when the player lets go of the jump key
-        if ((Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.Space)) && isJumping)
+        if (Input.GetKeyUp(KeyCode.W) && !isFalling)
         {
-            inputMovement = new Vector2(0, (.5f*jumpSpeed));
+            anim.Play("fall");
+            isFalling = true;
+            inputMovement = new Vector2(0, (rb.velocity.y/2));
+            sr.color = Color.yellow;
         }
 
         //Sideways movement
         if (Input.GetKey(KeyCode.A))
         {
-            if (rb.velocity.x > -maxSpeed) inputMovement += new Vector2(-acceleration, rb.velocity.y);
-            //if (rb.velocity.x <= -maxSpeed) inputMovement = new Vector2(-maxSpeed, 0);
-            //else inputMovement += new Vector2(-acceleration, 0);
+            anim.Play("walk");
+            if (rb.velocity.x > -maxSpeed) inputMovement += new Vector2(-acceleration, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            if (rb.velocity.x < maxSpeed) inputMovement += new Vector2(acceleration, rb.velocity.y);
-            //if (rb.velocity.x >= maxSpeed) inputMovement = new Vector2(maxSpeed, 0);
-            //else inputMovement += new Vector2(acceleration, 0);
+            if (rb.velocity.x < maxSpeed) inputMovement += new Vector2(acceleration, 0);
         }
         if (inputMovement == Vector2.zero)
+        {
+            anim.Play("idle");
             rb.sharedMaterial = frictionNotMoving;
+        }
         else
             rb.sharedMaterial = frictionMoving;
 
@@ -66,13 +72,10 @@ public class playerMovement : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
-            isJumping = false;
-    }
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
         {
             isJumping = false;
+            isFalling = false;
+            sr.color = Color.green;
         }
     }
 }
