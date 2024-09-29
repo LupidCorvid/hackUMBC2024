a244df495d91b908e0a53f00686ad3d49629c7f7
RJWMD <113476572+RJWMD@users.noreply.github.com>
2024-09-29 11:50:57 -0400

    Fixed boat and water in level2

--- New Unity Project (1)hackUMBC2024/Assets/Plants/BoatPlant.cs
+++ New Unity Project (1)hackUMBC2024/Assets/Plants/BoatPlant.cs
@@ -8,7 +8,10 @@ public class BoatPlant : Plant
     public override void followPlayer()
     {
         Vector2 newLocation = GameObject.FindGameObjectWithTag("Player").transform.position;
-        newLocation.y -= 1;
+        if(grown)
+            newLocation.y -= .5f;
+        else
+            newLocation.y += 1;
         rb.velocity = Vector2.zero;
         transform.position = newLocation;
     }
